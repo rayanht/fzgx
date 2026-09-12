a9b53925341ba810ce842d3c32f495f3cb66fdcc
Rayan Hatout <rayan.hatout@gmail.com>
2026-09-12 11:59:04 -0400

    tu-finish title: 64 declarations hoisted, 0 typedefs isolated, 132 contested, 4 unresolved

--- src/rel/title/_prolog.c
+++ src/rel/title/_prolog.c
@@ -1,4 +1,16 @@
 #include "types.h"
+#include "rel/title/globals.h"
+extern u32 fn_1_D0790(void);
+extern u8 lbl_8_bss_230;
+extern struct fn_8_784_lbl_1_bss_6EAD0 lbl_1_bss_6EAD0;
+extern struct fn_8_784_lbl_8_bss_22C lbl_8_bss_22C;
+extern u32 fn_1_5370(u32, u32);
+extern u32 fn_1_A1588(u32, u32);
+extern u32 lbl_8_bss_228;
+extern void fn_8_4D8(void);
+extern int fn_8_6FC(void);
+extern void fn_8_704(void);
+extern s32 fn_8_754(void);
 
 /* fzgx:begin fn_8_6FC */
 // fn_8_6FC: returns a constant.
@@ -8,9 +20,6 @@ int fn_8_6FC(void) {
 /* fzgx:end fn_8_6FC */
 
 /* fzgx:begin fn_8_754 */
-extern u32 fn_1_D0790(void);
-extern u8 lbl_8_bss_230;
-
 s32 fn_8_754(void) {
     lbl_8_bss_230 = 0;
     fn_1_D0790();
@@ -19,16 +28,6 @@ s32 fn_8_754(void) {
 /* fzgx:end fn_8_754 */
 
 /* fzgx:begin fn_8_784 */
-extern struct fn_8_784_lbl_1_bss_6EAD0 lbl_1_bss_6EAD0;
-extern struct fn_8_784_lbl_8_bss_22C lbl_8_bss_22C;
-extern u32 fn_1_5370(u32, u32);
-extern u32 fn_1_A1588(u32, u32);
-extern u32 lbl_8_bss_228;
-extern void fn_8_4D8(void);
-extern void fn_8_6FC(void);
-extern void fn_8_704(void);
-extern void fn_8_754(void);
-
 struct fn_8_784_lbl_8_bss_22C {
     u32 unk_0;
 };
